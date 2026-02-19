7dc392026e5fbf8f31bcf7affd957cd193789cd4
maxdiop349-oss <maxdiop349@gmail.com>
2026-02-19 15:35:13 +0000

    sdf

---
+++ src/composants/elements/section-Max.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function SectionMax() {
+  return (
+    <div className='bg-pink-800 h-scren'>
+        <h1 className='text-center text-pink-900 text-3xl font-bold'> Maguette</h1>
+        </div>
+
+  )
+}
--- src/composants/pages/home.tsx
+++ src/composants/pages/home.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import SectionLatyr from '../elements/sectrion-latyr'
+import SectionMax from '../elements/section-Max'
 
 export default function HomePage() {
   return (
-    <div className='bg-red-800 h-screen'>
+    <div className=' h-screen'>
       <SectionLatyr />
+      <SectionMax />
     </div>
   )
 }
