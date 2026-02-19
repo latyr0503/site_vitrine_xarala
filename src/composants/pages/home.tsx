7e54c209c847a9e9a411adb594e6fef076917b75
unknown <mamoudoufacko500@gamil.com>
2026-02-19 09:48:56 -0500

    modification de la taille de home

--- src/composants/pages/home.tsx
+++ src/composants/pages/home.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function HomePage() {
   return (
-    <div className='bg-red-800 h-screen'>Home</div>
+    <div className='bg-red-800 h-screen text-2xl'>Home</div>
   )
 }
