25417ca03e3bb8aa68037fadcc3d43bc504536c7
MBENGUE <mbenguef106@gmail.com>
2026-02-20 15:45:38 +0100

    Ajout de ma section jaune + mise en forme

--- src/composants/elements/Section-Fatou.tsx
+++ src/composants/elements/Section-Fatou.tsx
@@ -3,9 +3,7 @@ import React from 'react'
 export default function SectionFatou() {
   return (
     <div className='bg-yellow-800 h-screen'>
-        <h1 className='text-center text-white text-3xl font-bold'>SectionFatou
-
-        </h1>
+        <h1 className='text-center text-white text-3xl font-bold'>SectionFatou</h1>
         </div>
   )
 }
--- src/composants/elements/sectrion-latyr.tsx
+++ src/composants/elements/sectrion-latyr.tsx
@@ -2,19 +2,27 @@ import React from "react";
 
 export default function SectionLatyr() {
   return (
-    <div className="bg-green-800 p-6 space-y-6 ">
+    <div className="bg-yellow-800 p-6 space-y-6 ">
       <h1 className="text-center text-white text-2xl font-extrabold">A propose de moi </h1>
       <div className="grid grid-cols-2 gap-3">
-        <div>
-          <img
-            src="/assets/profil.png"
-            className="w-full h-[50%] object-contain rounded-2xl"
-            alt=""
-          />
+
+        <div className="w-full h-[50%] object-contain rounded-2xl bg-yellow-800
+            border border-amber-50 p-4 text-white ">
+          <h1 className="text-2xl">
+            Salut,</h1>
+            <h1 className="text-2xl">je suis  <span className="text-yellow-800 bg-white text-2xl">Fatou MBENGUE </span></h1>
+            Apprenant  a XARALA Academie en developpement full stack
+
+           <h2 className="text-2xl"> A propos de moi </h2> 
+             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Officiis magnam natus quo impedit sint eaque, </p>
+        
+            
+            
         </div>
         <div>
           <img
-            src="/assets/latyr.jpeg"
+            src="/assets/p1.jpeg"
             className="w-full h-[50%] object-cover rounded-2xl"
             alt=""
           />
