01b9e4f6b71cbc641ef1aae36be806baf47b5606
maxdiop349-oss <maxdiop349@gmail.com>
2026-02-20 01:29:16 +0000

    envoi de mon dernier modif

--- src/composants/elements/section-Max.tsx
+++ src/composants/elements/section-Max.tsx
@@ -3,8 +3,26 @@ import React from 'react'
 export default function SectionMax() {
   return (
     <div className='bg-pink-800 h-scren'>
-        <h1 className='text-center text-white
-         text-3xl font-bold'> Maguette</h1>
+        <h1 className="text-center text-white text-2xl font-extrabold">A propose de moi </h1>
+      <div className="grid grid-cols-2 gap-3">
+        <div>
+          {/* <img
+            src="/assets/profil.png"
+            className="w-full h-[50%] object-contain rounded-2xl"
+            alt=""
+          /> */}
+          <h1 className='text-center text-white text-2xl font-bold'>Salut;<br/>
+          <span className=''>Je suis Maguette Diop</span>
+          </h1>
+        </div>
+        <div>
+          <img
+            src="/assets/max.jpeg"
+            className="w-50 h-[50%] object-cover rounded-2xl"
+            alt=""
+          />
+        </div>
+      </div>
         </div>
 
   )
--- src/composants/pages/home.tsx
+++ src/composants/pages/home.tsx
@@ -1,11 +1,11 @@
 import React from "react";
-import SectionLatyr from "../elements/sectrion-latyr";
+
 import SectionMax from "../elements/section-Max";
 
 export default function HomePage() {
   return (
     <div className="">
-      <SectionLatyr />
+      {/* <SectionLatyr /> */}
       <SectionMax />
     </div>
   );
